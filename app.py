b985794b9ed02f98d3ee808fd5e0bae64a6391e6
Hakan Ardo <hakan@debian.org>
2018-04-28 06:56:49 +0200

    invite

--- app.py
+++ app.py
@@ -1,24 +1,57 @@
+import shelve
+from time import time
+
 from werkzeug.security import generate_password_hash, check_password_hash
 import connexion
 import os
 from base64 import b64encode
+import string
+import random
 
-users = {'hakan@debian.org': {'password': generate_password_hash('pass')}}
-active_tokens = {'z8XT5jpnmHgEo66nSctLJzRRpvTuhuOdDmNmp3+BU3I': 'hakan@debian.org'}
+##################################################################################
+#  Databases
+##################################################################################
+
+users = shelve.open("db/users")
+active_tokens = shelve.open("db/active_tokens")
+
+# FIXME: Remove expired tokens
+# FIXME: Mail invites
+# FIXME: Mode without auth for setting things up
+
+
+##################################################################################
+# User handling
+##################################################################################
 
 def login_password(credentials):
     uid = credentials['email']
     if uid in users:
         if check_password_hash(users[uid]['password'], credentials['password']):
             token = b64encode(os.urandom(32))[:-1].decode('utf-8')
-            active_tokens[token] = uid
+            active_tokens[token] = {'uid': uid, 'expires': time() + 48*60*60}
+            active_tokens.sync()
             return {'token': token}
     return 'Unauthorized', 401
 
 def token_info(token):
-    uid = active_tokens.get(token)
-    if uid is not None:
+    token_data = active_tokens.get(token)
+    if token_data is not None:
+        uid = token_data['uid']
         return {'uid': users[uid], 'scope': ['user']}
+
+def user_invite(who):
+    email = who['email']
+    individual = who['individual']
+    password = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
+    print(password)
+    users[email] = {'email': email, 'individual': individual, 'password': generate_password_hash(password)}
+    users.sync()
+
+def user_password():
+    pass
+
+##################################################################################
 
 def list_live_records(user):
     print(user)
--- cmds.sh
+++ cmds.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "email": "hakan@debian.org", "password": "pass"  }' 'http://0.0.0.0:8000/v1/login_password'
-curl -H 'Authorization: Bearer z8XT5jpnmHgEo66nSctLJzRRpvTuhuOdDmNmp3+BU3I' 'http://0.0.0.0:8000/v1/record'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "email": "hakan@debian.org", "individual": "xxx"  }' 'http://0.0.0.0:8000/v1/users/invite'
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "email": "hakan@debian.org", "password": "7tsLKBZo"  }' 'http://0.0.0.0:8000/v1/users/login_password'
+curl -H 'Authorization: Bearer SNyBsTTqghz6kGsUwl2b8YIyqwgjz893hCu80sM1xOU' 'http://0.0.0.0:8000/v1/records'
